1d0c1f3804757afdbc41c9080210d12670541bc7
Thiago Ferreira <thiagofer2003@outlook.com>
2025-03-13 18:35:33 -0300

    feat: more configs and update project

--- src/generate/dto/generate.dto.ts
+++ src/generate/dto/generate.dto.ts
@@ -1,15 +1,38 @@
-import { IsArray, IsBoolean, IsNumber, IsString } from 'class-validator';
+import {
+  IsBoolean,
+  IsInt,
+  IsObject,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+
+class MockField {
+  @ValidateNested({ each: true })
+  @Type(() => MockField)
+  fields: Record<string, MockField>;
+
+  @IsString()
+  type: string;
+
+  @IsObject()
+  params?: MockField;
+}
 
 export class GenerateDto {
   @IsString()
   locale?: string;
 
-  @IsNumber()
-  repeat?: number;
-
   @IsBoolean()
   show_errors?: boolean;
 
-  @IsArray()
-  mock: any[];
+  @IsInt()
+  repeat: number;
+
+  @IsBoolean()
+  single_item: boolean;
+
+  @ValidateNested()
+  @Type(() => MockField)
+  mock: MockField;
 }
