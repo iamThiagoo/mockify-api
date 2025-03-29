fa8e30a3cce9113a7566b02b8250d5e3d8a80cc3
Thiago Ferreira <thiagofer2003@outlook.com>
2025-03-28 23:55:15 -0300

    feat: support 'show_errors' param

--- src/generate/generate.service.ts
+++ src/generate/generate.service.ts
@@ -4,15 +4,28 @@ import { MockDto } from './dto/mock.dto';
 import { FakerLocale } from './enums/faker-locale.enum';
 import { allFakers, Faker } from '@faker-js/faker';
 import { FakerFieldMappings } from './constants/faker-mappings';
-
+import { ErrorsEnum } from './enums/errors.enum';
 export class GenerateService {
   generate(data: MockDto) {
-    const { locale = FakerLocale.EN, repeat = 1, mock } = data;
+    const {
+      locale = FakerLocale.EN,
+      repeat = 1,
+      mock,
+      show_errors = false,
+    } = data;
 
     const response = Array.from({ length: repeat }, () => {
       const data: Record<string, any> = {};
       for (const [fieldName, params] of Object.entries(mock)) {
-        data[fieldName] = this.generateData(locale, params.type);
+        const result = this.generateData(locale, params.type);
+
+        if (result === (ErrorsEnum.INVALID_TYPE as string)) {
+          if (!show_errors) continue;
+          if (!data['errors']) data['errors'] = {};
+          data['errors'][fieldName] = `'${params.type}' is not a valid type`;
+        } else {
+          data[fieldName] = result;
+        }
       }
       return data;
     });
@@ -24,6 +37,11 @@ export class GenerateService {
     try {
       const faker: Faker = allFakers[locale];
       const fakerType = FakerFieldMappings[type];
+
+      if (!fakerType) {
+        return ErrorsEnum.INVALID_TYPE;
+      }
+
       const [category, method] = fakerType.split('.');
 
       if (faker[category] && typeof faker[category][method] === 'function') {
